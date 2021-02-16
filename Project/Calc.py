904214a816cf9a662e6fc71fe6e163a7667c392c
Juliya <73654381+townofmask@users.noreply.github.com>
2021-02-17 01:31:25 +0500

    Update Calc.py

--- Project/Calc.py
+++ Project/Calc.py
@@ -1,30 +1,57 @@
 import re
-import operator
-
-OPERATORS = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
 
 class Calculator:
-    def calculate(self, s):
-        stack = []
-        stack2 = []
-        lst = list(s)
-        # stack = input().split()
+    def parse(self, s):
+        buffer = ''
+        sign = 1
+        operands = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',', '.')
+        znaki = ('+', '-', '*', '/','(', ')')
+        s = s.replace(' ', '')
+        s = s.replace(',', '.')
+        stack_num = []
+        stack_znaki = []
+        
         for element in s:
-            if element.isdigit():
-                stack.append(element)
-                lst.remove(element)
-            elif element in '+-*/':
-                stack2.append(element)
-                lst.remove(element)
+            if element in operands:
+                if sign == -1:
+                    element = int(element) * int(sign)
+                    element = str(element)
+                    sign = 1
+                if buffer == '':
+                    buffer = element
+                else:
+                    buffer += element
+            elif element in znaki:
+                if element in ["-"]:
+                        sign = -1
+                        element = "+"
+                stack_num.append(buffer)
+                buffer = ''
+                stack_znaki.append(element)
+
+        if buffer != '':
+            stack_num.append(buffer)
+        while stack_znaki != []:
+            a, b = stack_num.pop(), stack_num.pop()
+            znak = stack_znaki.pop()
+            a, b = float(b), float(a)
             
-        cnt1, cnt2 = stack.pop(), stack.pop()
-        operation = stack2.pop()
-        stack.append(OPERATORS[operation](int(cnt2), int(cnt1)))
-            # else:
-            #     cnt1, cnt2 = stack.pop(), stack.pop()
-            #     stack.append(OPERATORS[element](int(cnt2), int(cnt1)))
-            #     lst.remove(element)
-        return stack.pop()
+            def calculate(znak,a,b):
+                if znak == '+':
+                    return a + b
+                elif znak == '-':
+                    return a - b
+                elif znak == '*':
+                    return a*b
+                elif znak == '/':
+                    if b==0:
+                        return 'Деление на ноль!'
+                    else:
+                        return a/b 
+            result = calculate(znak,a,b) 
+            stack_num.append(result)
+        result = stack_num.pop()
+        return result
 
 print("Launching...")
 calc = Calculator()
@@ -34,53 +61,6 @@ while user_input not in ["exit"]:
     if regex != None:
         print("Please write an expression.")
     user_input = input("> ")
-    output = calc.calculate(user_input)
+    output = calc.parse(user_input)
     print(output)
 print("Closing...")
-            
-        # result = 0
-        # current = 0
-        # sign = 1
-        # stack = []
-        # mul = 0
-        # for ss in s:
-        #     if ss.isdigit():
-        #         current = int(ss)
-        #     elif ss in ["*", "/"]:
-        #         if ss == "*":
-        #             result = sign * current
-        #             current = 0
-        #             mul = 1
-        #         elif ss == "/":
-        #             result = current // sign
-        #             current = 0
-        #             mul = 2
-        
-
-        #     elif ss in ["-", "+"]:
-        #         result += sign * current
-        #         current = 0
-        #         mul = 0
-        #         if ss == "+":
-        #             sign = 1
-        #         else:
-        #             sign = -1
-        #     elif ss == "(":
-        #         stack.append(result)
-        #         stack.append(sign)
-        #         sign = 1
-        #         result = 0
-        #     elif ss == ")":
-        #         result += current * sign
-        #         result *= stack.pop()
-        #         result += stack.pop()
-        #         current = 0
-        
-        # if mul == 1:
-        #     return result * current
-        
-        # elif mul == 2:
-        #     return result // current
-
-        # else:       
-        #     return result + current * sign
